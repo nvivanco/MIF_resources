2d4b9ccddec49f13ad473e727db4c3e3ce441bc0
grinic <nicola.gritti.86@gmail.com>
2026-07-30 14:42:56 +0200

    Add template run script for PYMIF2ZARR workflow

---
+++ workflows/PYMIF2ZARR/template_run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=nf_launcher
+#SBATCH --partition=htc-el8
+#SBATCH --mem=16G
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/%x_%j_%u.out
+#SBATCH --error=logs/%x_%j_%u.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=user.name@embl.es # <----------------MODIFY
+
+# Load nextflow
+module load Nextflow/25.10.1
+
+# Navigate to repo
+cd /home/$USER
+
+#Using 'export' ensures these are visible to Nextflow sub-processes
+export CSV_PATH="/PATH/TO/batch.csv"           # <----------------MODIFY
+
+# Run Nextflow
+nextflow run mif_resources/workflows/PYMIF2ZARR/main.nf \ # <----------------MODIFY IF REPO IS CLONED IN DIFFERENT LOCATION
+    -i "$CSV_PATH" \
+    -resume
+
